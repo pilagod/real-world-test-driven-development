a44bfcf0faf3c7152aeb71bcb32895530118456e
pilagod <pilagooood@gmail.com>
2025-06-09 16:57:40 +0800

    use map in twoSum function to improve performance

--- example/twoSum/index.ts
+++ example/twoSum/index.ts
@@ -1,10 +1,12 @@
 export function twoSum(nums: number[], target: number): number[] {
-  for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j]
-      }
+  const diff: { [diff: number]: number } = {}
+
+  for (const [i, num] of nums.entries()) {
+    if (num in diff) {
+      return [diff[num], i]
     }
+    diff[target - num] = i
   }
+
   return []
 }
