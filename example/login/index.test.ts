8276faffaba56bf0818058b4811ed31bb00a2713
pilagod <pilagooood@gmail.com>
2025-06-13 16:23:25 +0800

    add login scenario description

---
+++ example/login/index.test.ts
@@ -0,0 +1,10 @@
+/**
+ * Login
+ *
+ * User can login with email and password, to get an access token for authentication.
+ */
+describe("Login", () => {
+  it("should pass this canary test", () => {
+    expect(true).toBe(true)
+  })
+})
